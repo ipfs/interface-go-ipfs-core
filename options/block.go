a2c0c403ce240fdaef68f45a4f3a63fe30c57297
Lucas Molas <schomatis@gmail.com>
2021-11-18 11:38:28 -0300

    feat(block options): add store codec

--- options/block.go
+++ options/block.go
@@ -3,10 +3,13 @@ package options
 import (
 	"fmt"
 	cid "github.com/ipfs/go-cid"
+	mc "github.com/multiformats/go-multicodec"
 	mh "github.com/multiformats/go-multihash"
 )
 
 type BlockPutSettings struct {
+	StoreCodec string
+	// FIXME: Rename to Format (and possibly mark as deprecated).
 	Codec    string
 	MhType   uint64
 	MhLength int
@@ -22,10 +25,11 @@ type BlockRmOption func(*BlockRmSettings) error
 
 func BlockPutOptions(opts ...BlockPutOption) (*BlockPutSettings, cid.Prefix, error) {
 	options := &BlockPutSettings{
-		Codec:    "",
-		MhType:   mh.SHA2_256,
-		MhLength: -1,
-		Pin:      false,
+		Codec:      "",
+		StoreCodec: "",
+		MhType:     mh.SHA2_256,
+		MhLength:   -1,
+		Pin:        false,
 	}
 
 	for _, opt := range opts {
@@ -35,33 +39,49 @@ func BlockPutOptions(opts ...BlockPutOption) (*BlockPutSettings, cid.Prefix, err
 		}
 	}
 
+	if options.Codec != "" && options.StoreCodec != "" {
+		return nil, cid.Prefix{}, fmt.Errorf("incompatible format (%s) and store-codec options set (%s)",
+			options.Codec, options.StoreCodec)
+	}
+
+	if options.Codec == "" && options.StoreCodec == "" {
+		// FIXME(BLOCKING): Do we keep the old default v0 here?
+		options.Codec = "v0"
+		// FIXME(BLOCKING): Review how to handle "protobuf". For now we simplify the code only with "v0".
+	}
+
 	var pref cid.Prefix
 	pref.Version = 1
 
-	if options.Codec == "" {
-		if options.MhType != mh.SHA2_256 || (options.MhLength != -1 && options.MhLength != 32) {
-			options.Codec = "protobuf"
-		} else {
-			options.Codec = "v0"
+	// Old format option.
+	if options.Codec != "" {
+		if options.Codec == "v0" {
+			if options.MhType != mh.SHA2_256 || (options.MhLength != -1 && options.MhLength != 32) {
+				return nil, cid.Prefix{}, fmt.Errorf("only sha2-255-32 is allowed with CIDv0")
+			}
+			pref.Version = 0
 		}
-	}
 
-	if options.Codec == "v0" && options.MhType == mh.SHA2_256 {
-		pref.Version = 0
-	}
-
-	formatval, ok := cid.Codecs[options.Codec]
-	if !ok {
-		return nil, cid.Prefix{}, fmt.Errorf("unrecognized format: %s", options.Codec)
-	}
-
-	if options.Codec == "v0" {
-		if options.MhType != mh.SHA2_256 || (options.MhLength != -1 && options.MhLength != 32) {
-			return nil, cid.Prefix{}, fmt.Errorf("only sha2-255-32 is allowed with CIDv0")
+		// FIXME(BLOCKING): Do we actually want to consult the CID codecs table
+		//  even with the old --format options? Or do we always want to check
+		//  the multicodec one?
+		cidCodec, ok := cid.Codecs[options.Codec]
+		if !ok {
+			return nil, cid.Prefix{}, fmt.Errorf("unrecognized format: %s", options.Codec)
 		}
+		pref.Codec = cidCodec
+	} else {
+		// New store-codec options. We handle it as it's done for `ipfs dag put`.
+		var storeCodec mc.Code
+		if err := storeCodec.Set(options.StoreCodec); err != nil {
+			return nil, cid.Prefix{}, err
+		}
+		pref.Codec = uint64(storeCodec)
 	}
 
-	pref.Codec = formatval
+	// FIXME: The entire codec manipulation/validation needs to be encapsulated
+	//  outside this funtion to clearly demark that it is the only option we are
+	//  overwriting here.
 
 	pref.MhType = options.MhType
 	pref.MhLength = options.MhLength
@@ -92,6 +112,15 @@ var Block blockOpts
 func (blockOpts) Format(codec string) BlockPutOption {
 	return func(settings *BlockPutSettings) error {
 		settings.Codec = codec
+		return nil
+	}
+}
+
+// StoreCodec is an option for Block.Put which specifies the multicodec to use to
+// serialize the object. It replaces the old Format now with CIDv1 as the default.
+func (blockOpts) StoreCodec(storeCodec string) BlockPutOption {
+	return func(settings *BlockPutSettings) error {
+		settings.StoreCodec = storeCodec
 		return nil
 	}
 }
